da9b1ff4197a1f77c85e35e81de6645262c8a865
wuxin <wuxin@kunteng.org>
2019-04-03 14:35:53 +0800

    1. add some dependencies

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,10 @@
 module.exports = {
   presets: ["@vue/app"]
+  /**
+   * iview按需引入时需要解开下面注释
+   */ 
+  // "plugins": [["import", {
+  //   "libraryName": "iview",
+  //   "libraryDirectory": "src/components"
+  // }]]
 };
--- src/main.js
+++ src/main.js
@@ -1,8 +1,15 @@
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
+import iView from "iview";
+// import { Split } from 'iview';
+
+import "iview/dist/styles/iview.css";
 
 Vue.config.productionTip = false;
+
+Vue.use(iView);
+// Vue.component('Split', Split);
 
 new Vue({
   router,
